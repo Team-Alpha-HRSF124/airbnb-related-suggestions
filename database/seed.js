ec03f8078d3d3697e77b1d3264df4c2ecc0ea58f
Felix Ding <felixding1989@gmail.com>
2019-12-01 13:52:14 -0800

    implement config file

--- database/seed.js
+++ database/seed.js
@@ -2,4 +2,11 @@ const db = require('./index.js');
 const path = require('path');
 const fs = require('fs');
 const Promise = require('bluebird');
+const pathToData = '../../scraper/data/page-details.txt';
 
+const fsAsync = Promise.promisifyAll(fs);
+
+fsAsync.readFile(path.resolve(__dirname, pathToData))
+  .then((data) => {
+    debugger;
+  })
